424a29d91f81342398f517f38842b1dedc93838a
Nuno Brito <brito.n@outlook.pt>
2021-11-16 11:46:48 +0000

    SS: TP1v1.7

--- SS/2122si/SS_TP1_v1.7.m
+++ SS/2122si/SS_TP1_v1.7.m
@@ -72,16 +72,16 @@ while aux
     switch escolha
         % A função solver recebe como pârametros o valor de k e a função xkt
         case 0
-            solver(0, x0t, -13, 5);
+            solver(0, x0t, -13, 5, 0,1,^,2,-1,v,0,0,^);
 
         case 1
-            solver(1, x1t, -2, 5);
+            solver(1, x1t, -2, 5, 0,0,^,0,0,^,0,0,^);
 
         case 2
-            solver(2, x2t, -8, 3);
+            solver(2, x2t, -8, 3, 0,-1,v,1,2,^,2,-1,v);
 
         case 3
-            solver(3, x3t,-3, 5);
+            solver(3, x3t,-3, 5, 0,-1,v,1,2,^,2,-1,v);
 
         otherwise
             disp('Valor inválido!')
@@ -117,7 +117,7 @@ x2t = x0t * sign(t-1);
 x3t = x1t + x2t;
 
 % Cálculo do produto interno
-prod_00 = double(int(x0t * x0t,t,0,2));  % < x0(t), x0(t) >
+prod_00 = double(int(x0t * x0t,t,0,2);  % < x0(t), x0(t) >
 prod_01 = double(int(x0t * x1t,t,0,2));  % < x0(t), x1(t) >
 prod_02 = double(int(x0t * x2t,t,0,2));  % < x0(t), x2(t) >
 prod_03 = double(int(x0t * x3t,t,0,2));  % < x0(t), x3(t) >
@@ -200,6 +200,8 @@ x3ag = subs(x3a,t,tg);
 
 disp('A base < x0t, x2t> não é ortogonal, pelo que os cálculos serão executados e os gráficos apresentados.')
 
+% Representação gráfica de funções
+figure
 subplot(3,2,1), hold on, plot(tg,x3tg,'b'), grid on, title('Função x3t'), xlabel('t (s)'), ylabel('Amplitude'), hold off;
 subplot(3,2,2), hold on, plot(tg,x0tg,'b'), grid on, title('Função x0t'), xlabel('t (s)'), ylabel('Amplitude'), hold off;
 subplot(3,2,3), hold on, plot(tg,x2tg,'b'), grid on, title('Função x2t'), xlabel('t (s)'), ylabel('Amplitude'), hold off;
@@ -210,7 +212,7 @@ subplot(3,2,5), hold on, plot(tg,x3ag,'b'), grid on, title('Função x3a'), xlab
 
 % Função solver que recebe o valor de k, xkt e parâmetros iniciais e finais do linspace
 
-function solver(k,x,inicial,final)
+function solver(k,x,inicial,final,s1,s2,sd1,s3,s4,sd2,s5,s6,sd3)
 
     % Parâmetros para linspace (para além dos que são recebidos pela função
     step = 1000;
@@ -243,7 +245,7 @@ function solver(k,x,inicial,final)
     uktg = subs(ukt,t,tg);
 
     % Cálculo da primitiva de Vk(t)
-    vkt = int(x, t);
+    vkt = double(int(x, t));
     vktg = subs(vkt,t,tg);
 
     % Construção de gráficos
@@ -252,7 +254,7 @@ function solver(k,x,inicial,final)
     subplot(3,2,2), hold on, plot(tg,zktfg,'b'), grid on, title(['Função z', num2str(k), 't']),  xlabel('t (s)'), ylabel('Amplitude'), hold off;
     subplot(3,2,3), hold on, plot(tg,xkptg,'b'), grid on, title(['Função xp', num2str(k), 't']), xlabel('t (s)'), ylabel('Amplitude'), hold off;
     subplot(3,2,4), hold on, plot(tg,xkitg,'b'), grid on, title(['Função xi', num2str(k), 't']), xlabel('t (s)'), ylabel('Amplitude'), hold off;
-    subplot(3,2,5), hold on, plot(tg,uktg,'b'),  grid on, title(['Função u', num2str(k), 't']),  xlabel('t (s)'), ylabel('Amplitude'), hold off;
+    subplot(3,2,5), hold on, plot(tg,uktg,'b'),  stem(s1,s2,sd1), stem(s3,s4,sd2), stem(s5,s6,sd3), grid on, title(['Função u', num2str(k), 't']),  xlabel('t (s)'), ylabel('Amplitude'), hold off;
     subplot(3,2,6), hold on, plot(tg,vktg,'b'),  grid on, title(['Função v', num2str(k), 't']),  xlabel('t (s)'), ylabel('Amplitude'), hold off;
 
 end
